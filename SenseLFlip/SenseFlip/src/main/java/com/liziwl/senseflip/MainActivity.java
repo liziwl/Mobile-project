baf5ca481f4423ee0f587567061b34c58966a6f4
liziwl <leezisy@gmail.com>
2020-05-27 16:28:20 +0800

    update name

--- SenseLFlip/SenseFlip/src/main/java/com/liziwl/senseflip/MainActivity.java
+++ SenseLFlip/SenseFlip/src/main/java/com/liziwl/senseflip/MainActivity.java
@@ -9,12 +9,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class Main2Activity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
