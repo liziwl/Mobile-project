4031c2cde70b060bb44ac174d209a365a19227a4
liziwl <leezisy@gmail.com>
2020-05-27 16:34:50 +0800

    refactor

--- SenseLFlip/SenseFlip/src/main/java/com/liziwl/senseflip/XYZ.java
+++ SenseLFlip/SenseFlip/src/main/java/com/liziwl/senseflip/XYZ.java
@@ -3,8 +3,6 @@ package com.liziwl.senseflip;
 import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class XYZ {
