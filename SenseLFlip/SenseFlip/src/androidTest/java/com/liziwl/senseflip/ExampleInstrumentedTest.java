84bc7896fa40085142d58650d5e6189e6a9ae8df
liziwl <leezisy@gmail.com>
2020-05-27 15:09:52 +0800

    update name

--- SenseLFlip/SenseFlip/src/androidTest/java/com/liziwl/senseflip/ExampleInstrumentedTest.java
+++ SenseLFlip/SenseFlip/src/androidTest/java/com/liziwl/senseflip/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.liziwl.senselist", appContext.getPackageName());
+        assertEquals("com.liziwl.senseflip", appContext.getPackageName());
     }
 }
