ba6343e69b9eea00058d88f000138bed498ea03d
liziwl <leezisy@gmail.com>
2020-05-27 12:39:21 +0800

    add msg

--- SenseLFlip/SenseFlip/src/main/java/com/liziwl/senseflip/ui/testBoard/TestBoardFragment.java
+++ SenseLFlip/SenseFlip/src/main/java/com/liziwl/senseflip/ui/testBoard/TestBoardFragment.java
@@ -1,6 +1,7 @@
 package com.liziwl.senseflip.ui.testBoard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,16 @@ import android.arch.lifecycle.ViewModelProviders;
 
 import com.liziwl.senseflip.R;
 
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class TestBoardFragment extends Fragment {
 
     private TestBoardViewModel testBoardViewModel;
+    final String JUDGE_URL = "http://127.0.0.1:5000/judge";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,5 +38,42 @@ public class TestBoardFragment extends Fragment {
         //     }
         // });
         return root;
+    }
+
+    public void sendPost() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(JUDGE_URL);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("timestamp", 1488873360);
+
+                    Log.i("JSON", jsonParam.toString());
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 }
