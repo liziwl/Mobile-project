c30dea36bf37404b5f9af08b607ea89f11a2bb7a
liziwl <leezisy@gmail.com>
2020-05-27 17:49:44 +0800

    add sample

--- judge/app.py
+++ judge/app.py
@@ -1,12 +1,43 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+sample_response = {
+    "passed": True,
+    "requester": "ziqiang",
+    "data": {
+        "1": {
+            "label": "ziqiang",
+            "score": 0.8
+        }
+    }
+}
+
+BAD_REQUEST = ({'code': 400, 'msg': 'Bad request'}, 400)
+
+
+class JudgeHandler(Resource):
+
+    def get(self):
+        return BAD_REQUEST
+
+    def post(self):
+        json_data = request.get_json()
+        if json_data:
+            return sample_response, 200
+        else:
+            return BAD_REQUEST
+
+
+api.add_resource(JudgeHandler, '/judge')
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'One Auth service'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
